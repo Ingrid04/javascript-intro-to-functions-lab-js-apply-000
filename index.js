e9e0b91ace4cde2f7fe51097759d5827a847122d
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-05 22:24:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function sayHiToGrandma (string) {
     return 'YES INDEED!';
   // Or else she responds with 'I love you, too.' to everything else
   } else if (string === 'I love you, Grandma.'){
-    return 'I love you,too.';
+    return 'I love you, too.';
   }
 }
