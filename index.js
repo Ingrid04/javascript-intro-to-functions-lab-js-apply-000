8030952b04adc476cc5844eada5b73fd9fade7ba
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-05 22:20:11 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout (string) {
+  return string.toUpperCase();
+}
+
+function whisper(STRING) {
+    return STRING.toLowerCase();
+}
+function logShout(string){
+const logShout = 'HELLO';
+console.log(logShout);
+}
+
+function logWhisper(string) {
+  const logWhisper = 'HELLO';
+  console.log('HELLO'.toLowerCase());
+}
+
+function sayHiToGrandma (string) {
+  if (string === 'Hello'){
+    'I can`t hear you!'.toLowerCase();
+ return 'I can`t hear you!';
+}
+}
+function sayHiToGrandma(string) {
+ var uppercase =  'YES INDEED!';
+ 'YES INDEED!'.toUpperCase();
+ return 'YES INDEED!';
+}
+function sayHiToGrandma(string){
+  if(string === "I Love You, Grandma."){
+    return 'I love you,too.';
+  }
+}
